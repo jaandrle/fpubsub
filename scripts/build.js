16f425e9575caa6262650e259ea2dc17c3ed72f4
Jan Andrle <andrle.jan@centrum.cz>
2023-01-06 13:41:26 +0100

    :boom:

--- scripts/build.js
+++ scripts/build.js
@@ -5,14 +5,23 @@ const path= join.bind(null, $[0], "..");
 const pathDist= path.bind(null, "..", "lib");
 const pkg= s.cat(path("..", "package.json")).xargs(JSON.parse);
 
-const keyword_export= "_public";
-const target= pathDist("cjs.js");
-echo("Building", { target }, "...");
-const cjm_content= s.cat(pathDist("esm.js"))
-	.sed("export const ", keyword_export+".")
-	.sed(/export( async)?( function) ([^\(]*)/, keyword_export+".$3= $3;\n$1$2 $3")
-	.split("\n").join("\n\t").trim();
-s.echo(`(function(module_name, factory){ /* jshint browser: true, node: true *//* global define:false */
+const target_cjs= pathDist("cjs.cjs");
+echo("Building", { target_cjs }, "...");
+const content= bundle(pathDist("esm.js"), pkg);
+content.to(target_cjs);
+
+const target_bundle= pathDist(pkg.name+".js");
+echo("Building", { target_bundle }, "...");
+content.to(target_bundle);
+$.exit(0);
+
+function bundle(path_source, pkg){
+	const keyword_export= "_public";
+	const cjm_content= s.cat(path_source)
+		.sed("export const ", keyword_export+".")
+		.sed(/export( async)?( function) ([^\(]*)/, keyword_export+".$3= $3;\n$1$2 $3")
+		.split("\n").join("\n\t").trim();
+	return s.echo(`(function(module_name, factory){ /* jshint browser: true, node: true *//* global define:false */
 	'use strict';
 	if (typeof define === 'function' && define.amd) {
 		define([], function(){
@@ -28,6 +37,5 @@ s.echo(`(function(module_name, factory){ /* jshint browser: true, node: true *//
 	const ${keyword_export}= {};
 	${cjm_content}
 	return Object.freeze(${keyword_export});
-});`)
-	.to(target);
-$.exit(0);
+});`);
+}
