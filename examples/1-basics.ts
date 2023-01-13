8879593354e5c3e702b3fd8cc3ec7cbb3a57c647
Jan Andrle <andrle.jan@centrum.cz>
2023-01-13 12:31:19 +0100

    :white_check_mark: :hammer:

--- examples/1-basics.ts
+++ examples/1-basics.ts
@@ -1,9 +1,19 @@
-import { topic, subscribe, publish } from '../lib/esm.js';
+import { topic, subscribe, publish, unsubscribe } from '../lib/esm.js';
 const log= (...before: string[])=> (...after: any[])=> console.log(...before, ...after);
 
 const simple_topic= topic<1|2>();
 log("Simple topic: `{}`:")(simple_topic);
 subscribe(simple_topic, log("Simple topic:"));
 subscribe(simple_topic, { handleEvent: log("Object listener:") });
+
+subscribe(simple_topic, log("Function listener (once):"), { once: true });
+
+const fNever1= log("Function listener (never):");
+subscribe(simple_topic, fNever1);
+unsubscribe(simple_topic, fNever1);
+const fNever2= log("Function listener (never):");
+subscribe(simple_topic, fNever2, { once: true });
+unsubscribe(simple_topic, fNever2);
+
 publish(simple_topic, 1);
 publish(simple_topic, 2);
